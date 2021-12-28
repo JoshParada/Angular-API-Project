043059332fca1ab49ed3c0cdfca2ad86338c4e6e
JoshParada <54327710+JoshParada@users.noreply.github.com>
2021-12-28 15:23:09 -0600

    deploy

--- APIProject/server.js
+++ APIProject/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/apiproject'));
+app.use(express.static(__dirname + '/dist/APIProject'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/apiproject/index.html'));});
+'/dist/APIProject/index.html'));});
 app.listen(process.env.PORT || 8080);
